13e6c17e7f0c16ab14d327c0179c45fc1e241188
milamelo4 <mel19009@byui.edu>
2022-04-25 22:04:29 -0600

    Update tictactoe.py
    Created the code for the game.
    Tomorrow ill try to use random method to computer choose a random number

--- tictactoe.py
+++ tictactoe.py
@@ -1,37 +1,62 @@
+import random
 print("Welcome to my Tic Tac Toe")
 
 
 def main():
-    pass
+    player = switch_player("") 
+    board = create_board()
+    while not (check_winner(board) or check_tie(board)):      
+      
+        print_board(board)
+        player_input(player, board)
+        player = switch_player(player) 
+        computer_turn(board)
+    
+    
+    print_board(board)
+    print(f"Thank you for playing.")
+    answer = input("Would you like to play again 'yes' or 'no? ")
+    while answer == "yes":
+        play_again(board) 
+    
+        break    
+ 
+        
+    
+def play_again(board):
+    player = switch_player("") 
+    board = create_board()
+    while not (check_winner(board) or check_tie(board)):      
+      
+        print_board(board)
+        player_input(player, board)
+        player = switch_player(player)     
+    
+    print_board(board)
+    answer = input("Thank you for playing. Would you like to play again 'yes' or 'no?")     
+        
+def create_board():
+    board = []
+    for slot in range(9):
+        board.append(slot + 1)
+    return board
 
-board = ["-", "-", "-",
-    "-", "-", "-",
-    "-", "-", "-"]
-
-
-current_player = "x"
-winner = None
 
 def print_board(board):
-    print(board[0] + " | " + board[1] + " | " + board[2]) 
+    print(f'{board[0]} | {board[1]} | {board[2]}') 
     print("----------")
-    print(board[3] + " | " + board[4] + " | " + board[5]) 
+    print(f'{board[3]} | {board[4]} | {board[5]}')  
     print("----------")
-    print(board[6] + " | " + board[7] + " | " + board[8])
+    print(f'{board[6]} | {board[7]} | {board[8]}') 
     print("----------") 
    
-#print_board(board)  
 
-     
-    
-def player_input(board):
-    user_input = int(input("Please enter a slot number between 1 through 9 or enter 'quit' to exit the program "))
-    if user_input >= 1 and user_input <=9 and board[user_input-1] == "-":
-        board[user_input-1] = current_player
-    else:
-        print("Slot already taken.")    
-        
-                  
+def check_tie(board):
+    for slot in range(9):
+        if board[slot] != "x" and board[slot] != "o":
+            return False
+    return True   
+
 def check_winner(board):
     return (board[0] == board[1] == board[2] or
             board[3] == board[4] == board[5] or
@@ -41,25 +66,39 @@ def check_winner(board):
             board[2] == board[5] == board[8] or
             board[0] == board[4] == board[8] or
             board[2] == board[4] == board[6])
+    
+    
+        
+def player_input(player, board):
+    user_input = int(input(f"{player}, Please enter a slot number between 1-9: "))
+    if user_input >= 1 and user_input <=9:
+        board[user_input-1] = player
+    else:
+        print("Slot already taken.")    
+ 
+def switch_player(current_player):
+    if current_player == "" or current_player == "o":
+        return "x"
+    elif current_player == "x":
+        return "o"        
+        
+def computer_turn(board, current_player):
+    while current_player == "0":
+        guess = random.randint(0, 8)
+        if board[guess] == "-":
+            board[guess] = "0"
+            switch_player()
+                          
+
      
-    
-    
-while True:    
-    print_board(board)
-    player_input(board)
 
     
-    
-   
 
-        
-   
-        
-                 
-            
+         
+       
+                  
         
         
         
-        
-#if __name__ == "__main__":
-   # main()
+if __name__ == "__main__":
+   main()
